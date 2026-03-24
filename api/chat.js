4d481a4910883cb1224013f22baa700cbb783e87
eliotjorge <eliotjorge@hotmail.com>
2026-03-24 16:46:56 +0000

    cambiada version de gemini

--- api/chat.js
+++ api/chat.js
@@ -39,7 +39,7 @@ URL: ${p.url}
       .join("\n\n");
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-1.5-flash-latest",
     });
 
     const result = await model.generateContent(`
